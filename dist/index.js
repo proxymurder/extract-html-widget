c81f42d37b761f3e5dc9689753d4b48ce6a38c4d
proxymurder <30883472+proxymurder@users.noreply.github.com>
2024-01-04 20:24:19 -0700

    parsed tables

--- dist/index.js
+++ dist/index.js
@@ -14,17 +14,61 @@ export const { extract_data } = {
 
         const html_widgets = d.querySelectorAll('div.html-widget');
 
-        let df = [];
-        html_widgets.forEach((e) => {
-            let id = e.getAttribute('id');
-            var script = d.querySelector(`script[data-for="${id}"]`);
+        let df = {
+            node: [],
+            table: [],
+        };
 
-            if (!script) return;
+        for (const i in html_widgets) {
+            let id = html_widgets[i].getAttribute('id');
+            var s = d.querySelector(`script[data-for="${id}"]`);
 
-            let { x } = JSON.parse(script.textContent || script.text || '{}');
+            if (!s) return null;
 
-            df.push(x);
-        });
+            let { x } = JSON.parse(s.textContent || s.text || '{}');
+
+            const o = x.options;
+
+            if (o.hasOwnProperty('columnDefs')) {
+                const defs = o.columnDefs;
+
+                df.table[i] = {
+                    keys: [],
+                    raw: [],
+                    size: 0,
+                };
+
+                for (const j in defs) {
+                    if (defs[j].name && defs[j].name != 'Id') {
+                        if (defs[j].name != ' ') {
+                            const name = `${defs[j].name}`
+                                .split('.')
+                                .join('_')
+                                .toLowerCase();
+
+                            const target = x.data[defs[j].targets];
+
+                            if (target.length != df.table[i].size) return null;
+
+                            df.table[i].keys.push(name);
+
+                            df.table[i][name] = {
+                                data: target,
+                            };
+
+                            df.table[i].raw = df.table[i].raw.map((r, i) => {
+                                return r + ';' + target[i];
+                            });
+                        } else {
+                            let target = x.data[defs[j].targets];
+
+                            df.table[i].size = target.length;
+                            df.table[i].raw = target;
+                        }
+                    }
+                }
+            }
+        }
 
         return df;
     },
